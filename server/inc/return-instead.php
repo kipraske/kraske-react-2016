d3c56a12d9c3ec7ac60cd01c1d9cebe1fb79a7e7
Kristofer Raske <kristofer.raske@wustl.edu>
2016-04-12 15:53:49 -0500

    Filtering output from hijack to be sanitized

--- server/inc/return-instead.php
+++ server/inc/return-instead.php
@@ -29,8 +29,21 @@ function hijack_main_loop_to_return_instead( $query ) {
 		// at pre-get-posts we haven't actually gotten the posts from the db yet
 		// So we need to manually fetch them here:
 		$return_query = new WP_Query($query->query);
+		$filtered_posts = array();
+		foreach( $return_query->posts as $post ){
+			$filtered_posts[] = array(
+				ID => (int) $post->ID,
+				comment_count => (int) $post->comment_count,
+				comment_status => $post->comment_status,
+				author_id => $post->post_author,
+				post_content => wp_kses_post($post->post_content),
+				post_date => $post->post_date,
+				post_excerpt => esc_html($post->post_excerpt),
+				post_title => esc_html($post->post_ttile),
+			);
+		}
 
-		echo json_encode($return_query->posts);
+		echo json_encode($filtered_posts);
 		die();
 	}
 }
