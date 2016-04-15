171c669b3a8e7f27153e97b22da7f904129e0dac
Kristofer Raske <kristofer.raske@wustl.edu>
2016-04-15 15:01:41 -0500

    Tweaking header in jsx as well

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,12 +4,15 @@ const Header = (props) => {
 	return (
 		<header id="masthead" className="site-header" role="banner">
 			<div className="site-logo">
-				**LOGO GOES HERE**
+			<a href="/" rel="home">
+				<img src="/wp-content/themes/kraske-react-2016/client/static/assets/site-logo.png" alt="Site Logo" />
+			</a>
 			</div>
 
 			<nav id="site-navigation" className="main-navigation" role="navigation">
 				<button className="menu-toggle" aria-controls="primary-menu" aria-expanded="false"></button>
-					**MENU GOES HERE**
+					<div className="menu-primary-container"> **MENU GOES HERE** </div>
+					<form className="search-form"> **SEARCHFORM GOES HERE** </form>
 			</nav>
 		</header>
 	);
