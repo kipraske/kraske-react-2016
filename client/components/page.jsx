759f52e0916ec2a2d0e9f8b14c04c279f9299f58
Kristofer Raske <kristofer.raske@wustl.edu>
2016-04-15 15:40:43 -0500

    Changing Page to be a smart class. It will control the color toggling

--- client/components/page.jsx
+++ client/components/page.jsx
@@ -7,35 +7,38 @@ var Single = require('./content/single.jsx');
 var None = require('./content/none.jsx');
 var Footer = require('./footer.jsx');
 
-const Page = (props) => {
-	var numberOfPosts = props.posts.length;
-	var contentElement;
-	if (numberOfPosts > 1) {
-		contentElement = [];
-		for (let post of props.posts){
-			contentElement.push(<Rollup post={post} key={post.id}/>);
-		}
-	} else if (numberOfPosts === 1) {
-		let post = props.posts[0];
-		contentElement = <Single post={post}/>;
-	} else {
-		contentElement = <None />;
-	}
+class Page extends React.Component {
 
-	return (
-		<div>
+	render(){
+		var numberOfPosts = this.props.posts.length;
+		var contentElement;
+		if (numberOfPosts > 1) {
+			contentElement = [];
+			for (let post of this.props.posts){
+				contentElement.push(<Rollup post={post} key={post.id}/>);
+			}
+		} else if (numberOfPosts === 1) {
+			let post = this.props.posts[0];
+			contentElement = <Single post={post}/>;
+		} else {
+			contentElement = <None />;
+		}
+
+		return (
+			<div>
 			<HeaderSkipLink />
 			<Header />
 			<div id="content" className="site-content">
-				<div id="primary" className="content-area">
-					<main id="main" className="site-main" role="main">
-						{contentElement}
-					</main>
-				</div>
+			<div id="primary" className="content-area">
+			<main id="main" className="site-main" role="main">
+			{contentElement}
+			</main>
+			</div>
 			</div>
 			<Footer />
-		</div>
-	);
+			</div>
+		);
+	}
 }
 
 module.exports = Page;
