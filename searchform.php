e51639424bcd9d70d1c3127ddd5f01bbec08034e
Kristofer Raske <kristofer.raske@wustl.edu>
2016-04-21 14:40:31 -0500

    Searchform styles complete

---
+++ searchform.php
@@ -0,0 +1,7 @@
+<form role="search" method="get" class="search-form" action="http://local.react.dev/">
+	<label>
+		<span class="screen-reader-text">Search for:</span>
+		<input type="search" class="search-field" placeholder="Search …" value="" name="s">
+	</label>
+	<button type="submit" class="search-submit">&#x1F50E</button>
+</form>
