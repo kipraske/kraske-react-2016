9e7853e3403d5676a05863378563368b4e59a8af
Kristofer Raske <kristofer.raske@wustl.edu>
2016-04-24 08:12:31 -0500

    Working on those query strings for the darn post nav

--- client/components/misc/post-navigation.jsx
+++ client/components/misc/post-navigation.jsx
@@ -1,7 +1,11 @@
 var React = require('react');
 
 const PostNavigation = (props) => {
-	var navigation_html = {__html:props.content}
+
+	// wordpress keeps the return_instead query var in the nav, but we don't want
+	// to link to that. So strip that out!
+	var navHTMLnoLinkQueryString = props.content.replace(/return_instead\=posts-json/g, "");
+	var navigation_html = {__html: navHTMLnoLinkQueryString}
 
 	return (
 		<nav className="prev-next-posts" dangerouslySetInnerHTML={navigation_html} />
