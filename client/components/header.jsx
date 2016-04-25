522cf043b6e14bf1aafb7272a7ac724ed5f8b338
Kristofer Raske <kristofer.raske@wustl.edu>
2016-04-25 16:20:41 -0500

    Adding title tag to menu thing

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,7 +4,7 @@ const Header = (props) => {
 	return (
 		<header id="masthead" className="site-header" role="banner">
 			<div className="site-logo">
-			<a href="/menu" rel="home">
+			<a href="/menu" title="Menu">
 				<img src="/wp-content/themes/kraske-react-2016/client/static/assets/site-logo.png" alt="Site Logo" />
 			</a>
 			</div>
